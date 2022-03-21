0ec3bf5d68cc652699c01dd0faefe1c9542887e7
BryceAMacLeod <W0436717@nscc.ca>
2022-03-21 07:45:04 -0300

    assignment 4B commented

--- Assignment_4_B/main.js
+++ Assignment_4_B/main.js
@@ -9,18 +9,23 @@
         fetch("https://threeinarowpuzzle.herokuapp.com/sample")
         .then((response) => response.json())
         .then((puzzleData) => {
+            // add puzzle to page
             buildPuzzle(puzzleData);
         })
-        
+        // takes in json containing three in a row puzzle data
         function buildPuzzle(puzzleData){
+            // checks if old data exists
             var oldPuzzle = document.querySelector('#thePuzzle')
             if((oldPuzzle) != null) {
+                // removes if exists
                 oldPuzzle.remove();
             }
             // creating container for puzzle
             var thePuzzle = document.createElement('div');
             thePuzzle.id = "thePuzzle";
+            // creating the table that holds the data
             var puzzleTable = document.createElement('table');
+            // table body / good practice
             var puzzleBody = document.createElement('tbody');
             puzzleBody.id = "puzzleBody"
             document.body.appendChild(thePuzzle);
@@ -42,6 +47,7 @@
                         puzzlePiece.addEventListener('click', puzzleClick, false)
                     }
                     else {
+                        // provided, untogglable squares highlighted with black border
                         puzzlePiece.style.border = "2px solid black";
                     }
                     switch(puzzleSquare.currentState) {
@@ -69,20 +75,31 @@
             thePuzzle.appendChild(puzzleTable);
 
             function puzzleClick() {
+                // from the unique ID of each td like 't#-#'
+                // #trow-column
                 let newString = this.id.split("t")
+                // row-column
                 let position = newString[1].split('-');
-                let x = parseInt(position[0]);
-                let y = parseInt(position[1]);
-                if(puzzleData.rows[x][y].currentState === 0) {
-                    puzzleData.rows[x][y].currentState++;
+
+                let row = parseInt(position[0]);
+                let column = parseInt(position[1]);
+
+                // if current state is gray/default/0
+                if(puzzleData.rows[row][column].currentState === 0) {
+                    puzzleData.rows[row][column].currentState++;
+                    // next colour is blue
                     this.style.backgroundColor = "rgb(0,0,255)";
                 }
-                else if(puzzleData.rows[x][y].currentState === 1) {
-                    puzzleData.rows[x][y].currentState++;
+                // current state of blue/1
+                else if(puzzleData.rows[row][column].currentState === 1) {
+                    puzzleData.rows[row][column].currentState++;
+                    // next state is yellow
                     this.style.backgroundColor = "rgb(255,255,0)";
                 }
-                else if(puzzleData.rows[x][y].currentState === 2) {
-                    puzzleData.rows[x][y].currentState = 0;
+                // current state of yellow/2
+                else if(puzzleData.rows[row][column].currentState === 2) {
+                    puzzleData.rows[row][column].currentState = 0;
+                    // next state is default/gray
                     this.style.backgroundColor = "rgb(128,128,128)";
                 }
             }
@@ -118,30 +135,41 @@
             // adding the 'check puzzle' button to the body
             thePuzzle.appendChild(checkPuzzleButton);
 
-            // creating the show incorrect squares checkbox
+            // creating the 'show incorrect squares' checkbox
             let checkSquaresBox = document.createElement('input');
             checkSquaresBox.type = "checkbox";
             checkSquaresBox.id = "checkbox";
             checkSquaresBox.style.marginLeft = 10 + "px";
-            let checkSquaresBoxLabel = document.createElement('label');
-            checkSquaresBoxLabel.htmlFor = "checkbox";
-            checkSquaresBoxLabel.innerText = "Show incorrect squares";
+
+            // fires on the 'change' event of the checkbox aka checking and unchecking
             checkSquaresBox.addEventListener('change', function () {
                 let rowCount = 0;
                 puzzleData.rows.map((row) => {
+                    // getting the squares that can be toggled
                     row.filter((square) => square.canToggle)
                     .map((square) => {
+                        // getting the square from the document
                         var theSquare = document.querySelector('#t' + rowCount + '-' + row.indexOf(square));
+                        // if the square's current state is wrong and not default
+                        // AND the state of the checkbox is checked
                         if(square.currentState !== square.correctState && square.currentState !== 0 && checkSquaresBox.checked) {
+                            // highlighting the square with a red border
                             theSquare.style.border = "2px solid red";
                         }
                         else {
+                            // setting the border back to default
                             theSquare.style.border = "inherit";
                         }
                     })
                     rowCount++;
                 })
             },false)
+
+            // creating a label for the checkbox
+            let checkSquaresBoxLabel = document.createElement('label');
+            checkSquaresBoxLabel.htmlFor = "checkbox";
+            checkSquaresBoxLabel.innerText = "Show incorrect squares";
+
             // adding the 'show incorrect square' checkbox to the body
             thePuzzle.appendChild(checkSquaresBox);
             thePuzzle.appendChild(checkSquaresBoxLabel);
@@ -155,6 +183,7 @@
                 .then((response) => response.json())
                 .then((newPuzzleData) => buildPuzzle(newPuzzleData));
             },false)
+
             // adding the 'new puzzle' button to the body
             thePuzzle.appendChild(newPuzzleButton);
         }
